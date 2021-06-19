28035f9039d3e85e438ccf10b6cda351e6c595cd
yassinerzk <yassinerazzouki@gmail.com>
2021-06-19 03:59:56 +0100

    finished redux setup and staring on form part

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {TextField, Button, Typography ,Color} from '@material-ui/core'
 
 import  useStyles  from './styles';
 
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { compose, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
-import {reducers} from './reducers';
+import {reducers} from './reducers'
 
 import App from './App';
 
